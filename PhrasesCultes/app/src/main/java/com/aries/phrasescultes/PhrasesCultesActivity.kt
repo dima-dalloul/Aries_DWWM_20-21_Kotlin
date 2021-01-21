1dff4e8dec833f96025e88ca6f489bfe7d806337
Dima <dima@dalloul.ch>
2021-01-21 14:07:55 +0100

    Cours 2 - Slide 50
    Méthode écho implémentée

--- PhrasesCultes/app/src/main/java/com/aries/phrasescultes/PhrasesCultesActivity.kt
+++ PhrasesCultes/app/src/main/java/com/aries/phrasescultes/PhrasesCultesActivity.kt
@@ -3,12 +3,16 @@ package com.aries.phrasescultes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import java.util.*
 
 class PhrasesCultesActivity : AppCompatActivity() {
+    val TAG = "PhrasesCultesActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,12 +20,11 @@ class PhrasesCultesActivity : AppCompatActivity() {
         // Initialisation de la variable liste
         var listePhrasesCultes : ArrayList<String> = arrayListOf<String>()
 
-
         listePhrasesCultes.addAll(resources.getStringArray(R.array.phrases_cultes_string_array))
 
         for(phrase in listePhrasesCultes) {
-            Log.i("PhrasesCultesActivity", phrase)
-            Toast.makeText(this, phrase, Toast.LENGTH_SHORT).show()
+            Log.i(TAG, phrase)
+            //Toast.makeText(this, phrase, Toast.LENGTH_SHORT).show()
         }
 
         // VERSION LONGUE
@@ -45,7 +48,24 @@ class PhrasesCultesActivity : AppCompatActivity() {
         val min = 0
         val max = listePhrasesCultes.size
         var phraseIndiceAleatoire = random.nextInt(max - min + 1)
-        Log.d("PhrasesCultesActivity", ""+phraseIndiceAleatoire)
+        Log.d(TAG, ""+phraseIndiceAleatoire)
         helloWorldTextView.setText(listePhrasesCultes.get(phraseIndiceAleatoire))
+
+
+        // Création d'un écho
+        val echoTextView : TextView = findViewById(R.id.echoTextView)
+        val echoEditText = findViewById<EditText>(R.id.echoInputUtilisateur)
+        val echoBouton = findViewById<Button>(R.id.echoBouton)
+
+        echoBouton.setOnClickListener{
+            Toast.makeText(this, "Echo", Toast.LENGTH_SHORT).show()
+            var texteDeLaPartDeLUtilisateur = echoEditText.text
+
+            Log.i(TAG, "l'Utilisateur a écrit : " + texteDeLaPartDeLUtilisateur)
+
+            echoTextView.setText(texteDeLaPartDeLUtilisateur)
+            echoTextView.setTextColor(ContextCompat.getColor(this, R.color.pink))
+            echoTextView.setTextSize(resources.getDimension(R.dimen.pageAccueilDimen))
+        }
     }
 }
